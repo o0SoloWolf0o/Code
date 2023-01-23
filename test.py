2a739f595f45ec178b09688bb99cabdc8cca96b8
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2023-01-23 09:26:03 +0700

    master

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import sys
 import numpy as np
-print(np.__version__)
-print()
+
 
 # print python version
 print(sys.version)
 print()
+
 # print python path
 print(sys.path)
+
+print(np.__version__)
+print()
+
+# Test WebHook GitHub
