4c8bdfffc44fcf7ba00660ba91801db08a0f2ec1
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2023-03-08 18:54:06 +0700

    master

--- CSS227/Lab3/js/Lab3_2.js
+++ CSS227/Lab3/js/Lab3_2.js
@@ -1,14 +1,20 @@
 function toggleMode(){
     var button = document.getElementsByTagName("button")[0];
-    var div = document.getElementsByClassName("myDIV")[0];
+    var head1 = document.getElementsByClassName("myDIV1")[0];
+    var head2 = document.getElementsByClassName("myDIV1")[1];
+    var div = document.getElementsByClassName("myDIV2")[0];
     if (button.innerHTML == "Active the night mode") {
         nightMode();
         button.innerHTML = "Active the day mode";
         div.innerHTML = "Click the button below to activate the day mode";
+        head1.innerHTML = "GOOD NIGHT";
+        head2.innerHTML = "This is night time!"
     } else {
         dayMode();
         button.innerHTML = "Active the night mode";
         div.innerHTML = "Click the button below to activate the night mode";
+        head1.innerHTML = "GOOD MORNING";
+        head2.innerHTML = "This is day time!";
     }
 }
 function nightMode() {
